54d4f342a059e8fff7e31a692005ec09159a1675
Alex Kentfield <alexkentfield@gmail.com>
2024-08-01 09:04:36 -0400

    minor gradle and lib update

--- app/src/main/java/com/example/weatherapp/ui/WeatherIcon.kt
+++ app/src/main/java/com/example/weatherapp/ui/WeatherIcon.kt
@@ -4,20 +4,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
 import coil.size.Scale
 
 @Composable
 fun WeatherIcon(iconId: String) {
     val url = "https://openweathermap.org/img/wn/$iconId.png"
 
-    val painter = rememberImagePainter(
-        data = url,
-        builder = {
-            scale(Scale.FIT) // Scale the image to fit into the layout
-        }
-    )
+    val painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(LocalContext.current).data(data = url).apply(block = fun ImageRequest.Builder.() {
+                scale(Scale.FIT) // Scale the image to fit into the layout
+            }).build()
+        )
 
     Box(modifier = Modifier.size(50.dp)) {
         Image(
